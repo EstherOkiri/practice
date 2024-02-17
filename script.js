ca26791afdd1a57cf38a852ae1d31cb0560f8953
EstherOkiri <est.okiri@gmail.com>
2024-02-17 14:02:37 -0800

    modify script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let num1 = 5;
 let num2 = 8;
 let add = num1 + num2;
+let sub = num2 - num1;
+let mult = num1 * num2;
 console.log(`The sum of ${num1} and ${num2} is ${add}`);
+console.log(`When you subtract ${num1} from ${num2} you get ${sub}`);
+console.log(`When you multiply ${num1} by ${num2} you get ${mult}`);
